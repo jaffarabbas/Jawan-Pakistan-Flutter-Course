f7524ab27272d792a969dcfc7a872db1dd92df4a
jaffarabbas <gamaportal8@gmail.com>
2021-09-08 00:15:49 +0500

    9-8-2021

---
+++ Dart-Practice/2-Variables/basic_variable_syntax.dart
@@ -0,0 +1,15 @@
+/*
+var is a variable syntax that store every data type
+*/
+
+//<datatype name> <variable name> <assignment operator> <value>
+var number = 3;
+var number2 = 4;
+//temp variable is a naming convention that is use for temprory variable
+//temporary varibale: those variables that are use for storing values temporary
+var temp;
+void main() {
+  temp = number + number2;
+  print(number);
+  print(temp);
+}
