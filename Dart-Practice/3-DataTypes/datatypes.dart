25cea56c6c80f25d59fa9822d01b6517697ee168
jaffarabbas <gamaportal8@gmail.com>
2021-09-09 22:10:48 +0500

    9-9-2021

---
+++ Dart-Practice/3-DataTypes/datatypes.dart
@@ -0,0 +1,14 @@
+/* 
+String :Any data in “ “ is called string e.g “jaffar”
+Syntax: String
+int : a data type that stores a simple number is called int
+Syntax: int 
+num: a data type which store both int and double its like var
+Syntax: num
+double : a data type that stores decimal value or integer
+Syntax: double
+var: a data type that stores any data type value in itself
+Syntax: var
+bool: a data type that stores any 0 and 1 true and false
+Syntax: bool
+*/
