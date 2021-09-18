0d52bc78079280fd485348a0ecfd74381c654302
jaffarabbas <gamaportal8@gmail.com>
2021-09-18 19:41:05 +0500

    9-18-2021

---
+++ Assignments/2-Assignement-(Multiple-Questions)/Question-3.dart
@@ -0,0 +1,8 @@
+// (3)Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a
+// variable & calculate the cost of buying 5 tickets to a movie.
+
+void main(){
+  int ticket = 600;
+  int CostOfFiveTicket = ticket * 5;
+  print("Cost of Single Ticket : ${ticket} PKR \nCost of 5 tickets are : ${CostOfFiveTicket} PKR");
+}
