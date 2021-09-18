6040e187c20e042bb9e9d00017d4cca3c053b472
jaffarabbas <gamaportal8@gmail.com>
2021-09-18 23:25:29 +0500

    9-18-2021

---
+++ Assignments/2-Assignement-(Multiple-Questions)/Question-12.dart
@@ -0,0 +1,34 @@
+// (12) Write a program to generate your K-Electric bill 7. All the amounts should
+// be rounded off to 2 decimal places. Display the following fields:
+// a. Customer Name
+// b. Current Month
+// c. Number of units
+// d. Charges per unit
+// e. Net Amount Payable (within Due Date)
+// f. Late Payment Surcharge
+// g. Gross Amount Payable (after Due Date)
+
+// Where, Net Amount Payable (within Due Date) = Number of units * Charges per unit
+// & Gross Amount Payable (after Due Date) = Net Amount + Late Payment Surcharge
+String CustumerName = "Jaffar Abbas";
+String CurrentMonth = "September";
+num PricesPerUnit = 120;
+num NumberOfUnits = 300;
+String Date = "19-9-2021";
+String DueDate = "1-10-2021";
+num LateCharges = 70;
+void main() {
+  num NetAmountPayableWithInDueDate = NumberOfUnits * PricesPerUnit;
+  num NetAmountPayableAfterDueDate = NetAmountPayableWithInDueDate + LateCharges;
+
+  print("Customer Name : $CustumerName\n"+
+  "Current Month : $CurrentMonth\n"+
+  "Number Of Units Consume : $NumberOfUnits\n"+
+  "Current Month : $CurrentMonth\n"+
+  "Due Date : $DueDate\n"+
+  "Unit price: $PricesPerUnit\n"+
+  "Price WithIn DueDate : $NetAmountPayableWithInDueDate\n"+
+  "Price After DueDate : $NetAmountPayableAfterDueDate\n"+
+  "Late Charges : $LateCharges"
+  );
+}
