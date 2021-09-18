5ae32a7d74c2e399e33a3bbe8ae86c6c41b43dd7
jaffarabbas <gamaportal8@gmail.com>
2021-09-18 23:25:35 +0500

    9-18-2021

---
+++ Assignments/2-Assignement-(Multiple-Questions)/Question-13.dart
@@ -0,0 +1,15 @@
+// Write a program that shows the message “First fifteen days of the month”
+// if the date is less than 16th of the month else shows “Last days of the
+// month”.
+
+
+void main() {
+  String Date = "14-Sep-2021";
+  int Day = int.parse(Date.substring(0,2));
+  print("Date : $Date");
+  if(Day < 16){
+    print("First fifteen days of the month");
+  }else{
+    print("Last days of the month");
+  }
+}
