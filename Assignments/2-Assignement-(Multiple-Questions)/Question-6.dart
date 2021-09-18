b133690ca2acc9dfac12122c637b4d179071112c
jaffarabbas <gamaportal8@gmail.com>
2021-09-18 15:27:13 +0500

    9-18-2021

---
+++ Assignments/2-Assignement-(Multiple-Questions)/Question-6.dart
@@ -0,0 +1,26 @@
+// (6)What are the data types supported in Dart? Explain with Examples.
+
+
+// String :Any data in “ “ is called string 
+// Syntax: String
+// e.g String a = “jaffar” : jaffar is String
+// int : a data type that stores a simple number is called int
+// Syntax: int 
+// e.g int a = 4; : 4 is int
+// num: a data type which store both int and double its like var
+// Syntax: num
+// e.g num a = 4; : 4 is int or num a = 4.4 : double
+// double : a data type that stores decimal value or integer
+// Syntax: double
+// e.g double a = 4.4; : 4.3 is double
+// var: a data type that stores any data type value in itself
+// Syntax: var
+// bool: a data type that stores any 0 and 1 true and false
+// Syntax: bool
+// e.g bool a = true; : true is bool
+// list : it isa group of objects
+// Syntax: var list = [indexs,indexs]
+// eg var list = [1,3,4];
+// Map: set of values as key and value pair like json
+// Syntax: var maps = {key : value}
+// e.g :  var mapp = {1:"one",2:"two",3:"three"}
