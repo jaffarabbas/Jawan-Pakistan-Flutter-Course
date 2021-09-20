6559ab8132c8a22ac450ed3f4e66450e3140bc22
jaffarabbas <gamaportal8@gmail.com>
2021-09-20 16:39:18 +0500

    9-20-2021

---
+++ Dart-Practice/7-Loops/forLoop.dart
@@ -0,0 +1,9 @@
+void main() {
+  int n = 5,j = 5;
+  for(int i = 0; i<n; i++) {
+    int reverse = ((((i<=j)?(j--):j)));
+    var nums = '*',spaces = " ";
+    //  print('${reverse } , $i ');
+    print(spaces * i);
+  }
+}
