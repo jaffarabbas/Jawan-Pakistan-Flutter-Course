e3649e1bb25dcd7e6164cd89b9eef8702a1a97f6
jaffarabbas <fa19bsse0008@maju.edu.pk>
2021-09-25 23:12:51 +0500

    25/09/2021 23:12:50

--- Assignments/3-Assignment-(List-and-Map)/Question-10.dart
+++ Assignments/3-Assignment-(List-and-Map)/Question-10.dart
@@ -2,10 +2,30 @@
 // and returns true if it is a vowel, false otherwise.
 import 'dart:io';
 
+/*
+Note : we can solve this question with list own functions below are 2 more ways to solve this question
+from list generated for each loop
+vowel.forEach((element) {
+  if(element == character[0]){
+    isContainVowel = true;
+  }
+}); 
+from list generated function (it will check the character in the list)
+isContainVowel = vowel.contains(character[0]);
+*/
+
 void main() {
+  bool isContainVowel = false;
   var vowel = ['a','e','i','o','u'];
+  print("Enter Character");
   String? character = stdin.readLineSync()!;
-  bool isContainVowel = vowel.contains(character[0]);
+  //from for loop itration
+  for(int i=0; i < vowel.length; i++){
+    if(character[0] == vowel[i]){
+      isContainVowel = true;
+      break;
+    }
+  }
   if(isContainVowel){
     print('$isContainVowel it is a Vowel');
   }else{
