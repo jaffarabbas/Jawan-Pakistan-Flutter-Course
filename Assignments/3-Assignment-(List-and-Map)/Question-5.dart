5a7cab579c219ffa04c5ee221a2e3f7e1d4a7304
jaffarabbas <fa19bsse0008@maju.edu.pk>
2021-09-29 19:38:25 +0500

    9-29-2021

--- Assignments/3-Assignment-(List-and-Map)/Question-5.dart
+++
@@ -1,8 +0,0 @@
-// 5. Write a program to print multiplication table of 7 length 15.
-
-void main() {
-  int number = 7 , range = 15;
-  for(int i = 1; i <= range ; i++) {
-    print("$number * $i = ${number*i}");
-  }
-}
