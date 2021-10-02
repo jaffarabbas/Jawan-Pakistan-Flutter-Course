6ef98750d96fbcc4241209fcc0313c0865a9f99f
jaffarabbas <gamaportal8@gmail.com>
2021-10-02 22:18:54 +0500

    02/10/2021 22:18:53

--- Assignments/4-Assignment-(oop)/Question-8.dart
+++ Assignments/4-Assignment-(oop)/Question-8.dart
@@ -3,20 +3,42 @@ import 'dart:math';
 import 'dart:io';
 
 class AccountGenerate {
-  Map AccountCollections = {};
+  List AccountCollections = [];
   //new account number
   String NewAccountNumber(){
     var random = new Random();
-    var AccountNumber;
+    var AccountNumber = '';
     for (var i = 0; i < 10; i++) {
-      AccountNumber = random.nextInt(1000);
+      AccountNumber += random.nextInt(1000).toString();
     }
     return AccountNumber;
-  }
+  } 
 }
 
 class UserInformation extends AccountGenerate{
-  void NewUser(var name,var AccountNumber){
-    super.AccountCollections.add({'name':name,'AccountNumber':AccountNumber});
+  void NewUser(var name){
+    super.AccountCollections.add([name,this.NewAccountNumber()]);
+  }
+}
+
+void main() {
+  //UserInformation object
+  UserInformation newUser = new UserInformation();
+  while(true){
+    print('Want to enter user yes or not y/n\nprint list p');
+    var operater = stdin.readLineSync()!;
+    if(operater == 'n'){
+      break;
+    }else if(operater == 'y'){
+     print("Enter new Name for account Creation: ");
+     String name = stdin.readLineSync()!;
+     newUser.NewUser(name);
+    }else if(operater == 'p'){
+      newUser.AccountCollections.forEach((element) {
+        print("Account Holder Name : ${element[0]}\nAccount Number : ${element[1]}");
+      });
+    }else{
+      print('Error');
+    }
   }
 }
