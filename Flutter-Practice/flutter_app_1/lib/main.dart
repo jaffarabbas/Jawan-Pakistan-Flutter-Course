7b4f7d7691e0988899ebe687282af670f955d66f
jaffarabbas <gamaportal8@gmail.com>
2021-10-04 10:45:58 +0500

    04/10/2021 10:45:58

--- Flutter-Practice/flutter_app_1/lib/main.dart
+++ Flutter-Practice/flutter_app_1/lib/main.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
-import 'package:flutter_app_1/class-work/work-1.dart';
-import 'package:flutter_app_1/class-work/work-2.dart';
+import 'package:flutter_app_1/class-work/class-6/work-2.dart';
+// import 'package:flutter_app_1/class-work/widgets/work-1.dart';
+// import 'package:flutter_app_1/class-work/widgets/work-2.dart';
 
 //it is a main function that call the runApp() in it
 void main() {
