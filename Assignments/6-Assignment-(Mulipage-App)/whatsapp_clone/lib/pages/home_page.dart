f7b1830916e288b88fb12f7295ba65723a99fc7b
jaffarabbas <gamaportal8@gmail.com>
2021-10-15 17:39:07 +0500

    15/10/2021 17:39:07

---
+++ Assignments/6-Assignment-(Mulipage-App)/whatsapp_clone/lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WHATSAPP'),
+      ),
+      body: Text("sakjhkjashd"),
+    );
+  }
+}
