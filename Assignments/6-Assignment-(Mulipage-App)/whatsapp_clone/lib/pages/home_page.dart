13b4e0db900c841c993514214325824b64527ed6
jaffarabbas <gamaportal8@gmail.com>
2021-10-15 22:52:37 +0500

    15/10/2021 22:52:37

--- Assignments/6-Assignment-(Mulipage-App)/whatsapp_clone/lib/pages/home_page.dart
+++ Assignments/6-Assignment-(Mulipage-App)/whatsapp_clone/lib/pages/home_page.dart
@@ -11,13 +11,55 @@ class HomePage extends StatelessWidget {
       length: 4,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('WHATSAPP'),
+          title: Text(
+            'WhatsApp',
+            style: TextStyle(
+              fontSize: 22
+            )
+          ),
+          actions: [
+            Icon(
+              Icons.search,
+              size: 27,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Icon(
+              Icons.more_vert,
+              size: 27,
+            ),
+            SizedBox(
+              width: 20,
+            )
+          ],
           bottom: TabBar(
             tabs: [
-              Tab(icon: Icon(Icons.camera_enhance),),
-              Tab(text : 'CHATS'),
-              Tab(text : 'STATUS'),
-              Tab(text : 'CALLS'),
+              Tab(icon: Icon(Icons.camera_alt_rounded),),
+              Tab(
+                child: Text(
+                  'CHATS',
+                  style: TextStyle(
+                    fontSize: 16
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  'STATUS',
+                  style: TextStyle(
+                    fontSize: 16
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  'CALLS',
+                  style: TextStyle(
+                    fontSize: 16
+                  ),
+                ),
+              ),
             ],
           ),
         ),
