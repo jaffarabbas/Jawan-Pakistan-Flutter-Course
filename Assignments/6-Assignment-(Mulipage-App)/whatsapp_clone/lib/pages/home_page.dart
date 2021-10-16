c026a4eb5f1392d8b73b52230c9831e21fbba7d1
jaffarabbas <gamaportal8@gmail.com>
2021-10-16 21:17:22 +0500

    16/10/2021 21:17:21

--- Assignments/6-Assignment-(Mulipage-App)/whatsapp_clone/lib/pages/home_page.dart
+++ Assignments/6-Assignment-(Mulipage-App)/whatsapp_clone/lib/pages/home_page.dart
@@ -1,23 +1,21 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/pages/call_page.dart';
 import 'package:whatsapp_clone/pages/chat_list.dart';
+import 'package:whatsapp_clone/pages/status_page.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({ Key? key }) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
+      initialIndex: 1,
       length: 4,
       child: Scaffold(
         appBar: AppBar(
-          title: Text(
-            'WhatsApp',
-            style: TextStyle(
-              fontSize: 22
-            )
-          ),
+          title: Text('WhatsApp', style: TextStyle(fontSize: 22)),
           actions: [
             Icon(
               Icons.search,
@@ -36,29 +34,25 @@ class HomePage extends StatelessWidget {
           ],
           bottom: TabBar(
             tabs: [
-              Tab(icon: Icon(Icons.camera_alt_rounded),),
+              Tab(
+                icon: Icon(Icons.camera_alt_rounded),
+              ),
               Tab(
                 child: Text(
                   'CHATS',
-                  style: TextStyle(
-                    fontSize: 16
-                  ),
+                  style: TextStyle(fontSize: 16),
                 ),
               ),
               Tab(
                 child: Text(
                   'STATUS',
-                  style: TextStyle(
-                    fontSize: 16
-                  ),
+                  style: TextStyle(fontSize: 16),
                 ),
               ),
               Tab(
                 child: Text(
                   'CALLS',
-                  style: TextStyle(
-                    fontSize: 16
-                  ),
+                  style: TextStyle(fontSize: 16),
                 ),
               ),
             ],
@@ -66,10 +60,10 @@ class HomePage extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
+            Text('camera 404'),
             ChatList(),
-            ChatList(),
-            ChatList(),
-            ChatList(),
+            AppStatusPage(),
+            AppCallPage(),
           ],
         ),
       ),
