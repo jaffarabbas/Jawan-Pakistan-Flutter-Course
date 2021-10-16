295d388f5f0bd1a284d0bf12c93cfb92492d4438
jaffarabbas <gamaportal8@gmail.com>
2021-10-16 18:51:51 +0500

    16/10/2021 18:51:51

--- Assignments/6-Assignment-(Mulipage-App)/whatsapp_clone/lib/pages/chat_list.dart
+++ Assignments/6-Assignment-(Mulipage-App)/whatsapp_clone/lib/pages/chat_list.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: non_constant_identifier_names, avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/dataSource/dataMap.dart';
 import 'package:whatsapp_clone/widgits/AppListTile.dart';
 
 class ChatList extends StatefulWidget {
@@ -9,13 +12,20 @@ class ChatList extends StatefulWidget {
 }
 
 class _ChatListState extends State<ChatList> {
+  //populate list with map
+  List<Widget> Populate(){
+    List<Widget> lst = [];
+    for(int i = 0; i < DataSource.Datamap()["friendsList"].length;i++){
+      lst.add(AppChatTile(userInformation : DataSource.Datamap()["friendsList"][i]));
+    }
+    print(lst);
+    return lst;
+  }
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
-        children: [
-          AppChatTile(username: 'jaffar',userImage: '',userRecentMessage: 'hello',userLastSeen: '',)
-        ],
+        children: Populate(),
       ),
     );
   }
