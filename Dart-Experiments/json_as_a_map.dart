107ded24cc526e91b7f21a1096704b3ae1fa7092
jaffarabbas <gamaportal8@gmail.com>
2021-10-16 18:51:51 +0500

    16/10/2021 18:51:51

--- Dart-Experiments/json_as_a_map.dart
+++ Dart-Experiments/json_as_a_map.dart
@@ -2,32 +2,40 @@
 
 void main() {
   Map map = {
-    "friendsList":{
-      1:{
-        "firendId":1,
-        "image":"",
-        "username":"gfg",
+    "friendsList": {
+      0: {
+        "firendId": 1,
+        "image": "",
+        "username": "gfg",
       },
-      2:{
-        "firendId":2,
-        "image":"",
-        "username":"",
+      1: {
+        "firendId": 2,
+        "image": "",
+        "username": "Ahmed",
       }
     },
-    "chatList":{
-      1:{
-          "mainUser":"'username'+'id'",
-          "chatUser":"'username'+'id'",
-          "messsges":{
-             "mainUser+messageid":{
-               "message":"",
-               "timestamps":"",
-             },
-             "chatUser+messageid":{
-               "message":"",
-               "timestamps":"",
-             }
+    "chatList": {
+      1: {
+        "mainUser": {
+          "firendId": 1,
+          "image": "",
+          "username": "gfg",
+        },
+        "chatUser": {
+          "firendId": 2,
+          "image": "",
+          "username": "",
+        },
+        "messsges": {
+          "mainUser+messageid": {
+            "message": "",
+            "timestamps": "",
+          },
+          "chatUser+messageid": {
+            "message": "",
+            "timestamps": "",
           }
+        }
       }
     }
   };
@@ -35,5 +43,9 @@ void main() {
   // map.forEach((key, value) {
   //   print(value);
   // });
-  print(map["friendsList"][1]["username"]);
+ // print(map["friendsList"][1]["username"]);
+
+  for(int i = 0; i < map["friendsList"].length;i++){
+     print(map["friendsList"][i]);
+  }
 }
