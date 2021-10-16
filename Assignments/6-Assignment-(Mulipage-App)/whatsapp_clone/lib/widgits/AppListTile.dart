7a2e842ae8f24961b708e6c9909c18a63f56b9fd
jaffarabbas <gamaportal8@gmail.com>
2021-10-16 21:17:22 +0500

    16/10/2021 21:17:21

--- Assignments/6-Assignment-(Mulipage-App)/whatsapp_clone/lib/widgits/AppListTile.dart
+++ Assignments/6-Assignment-(Mulipage-App)/whatsapp_clone/lib/widgits/AppListTile.dart
@@ -1,7 +1,7 @@
-// ignore_for_file: prefer_const_constructors_in_immutables, file_names, prefer_initializing_formals, must_be_immutable, prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors_in_immutables, file_names, prefer_initializing_formals, must_be_immutable, prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
-import 'package:whatsapp_clone/pages/chat_page.dart';
+import 'package:whatsapp_clone/utils/routes.dart';
 
 class AppChatTile extends StatefulWidget {
   Map<dynamic, dynamic> userInformation;
@@ -15,23 +15,30 @@ class AppChatTile extends StatefulWidget {
 }
 
 class _AppChatTileState extends State<AppChatTile> {
+  void ChatScreen(BuildContext context){
+    Navigator.pushNamed(context, AppRoutes.chatRoute);
+  }
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => ChatScreen(),
-      child: ListTile(
+    return ListTile(
+        onTap:() => ChatScreen(context),
         title: Text(widget.userInformation["username"]),
-        subtitle: Text(''),
+        subtitle: Text('hii jaffar'),
         leading: CircleAvatar(
           radius: 25,
           backgroundImage: NetworkImage(widget.userInformation["image"]),
         ),
         trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(''),
+            Text(
+              '00:00',
+              style: TextStyle(
+                fontSize: 12
+              ),
+            ),
           ],
         ),
-      ),
-    );
+      );
   }
 }
