82d48c34bbbeb7a34a8b0ad81306776b0c5b3574
jaffarabbas <gamaportal8@gmail.com>
2021-10-17 23:17:27 +0500

    17/10/2021 23:17:26

--- Quizes/ecommerce_cart_ui/lib/pages/home_page.dart
+++ Quizes/ecommerce_cart_ui/lib/pages/home_page.dart
@@ -1,22 +1,40 @@
-// ignore_for_file: prefer_const_constructors_in_immutables, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors_in_immutables, avoid_unnecessary_containers, prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names
 
+import 'package:ecommerce_cart_ui/DataSource/DataMap.dart';
+import 'package:ecommerce_cart_ui/widgits/cart.dart';
 import 'package:ecommerce_cart_ui/widgits/header.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({ Key? key }) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  Widget CartTileView() {
+    return ListView.builder(
+        itemCount: Datamap.DataSource().length,
+        itemBuilder: (context, index) {
+          return CartTile(productInformation: Datamap.DataSource()[index]);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      alignment: Alignment.center,
       child: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Header(),
+          Expanded(
+            child: SizedBox(
+              width: 340,
+              child: CartTileView()
+            ),
+          )
         ],
       ),
     );
