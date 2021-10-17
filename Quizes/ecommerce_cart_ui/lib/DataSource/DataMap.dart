8c2c434e12c10a3e4def11fe39592095cd397917
jaffarabbas <gamaportal8@gmail.com>
2021-10-17 23:17:27 +0500

    17/10/2021 23:17:26

---
+++ Quizes/ecommerce_cart_ui/lib/DataSource/DataMap.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: non_constant_identifier_names, file_names
+
+class Datamap {
+  static DataSource() {
+    return {
+      0: {
+          "productId": 0,
+          "productImage": 'Images/p3.png',
+          "productName": 'Men`s\nFuelSell Echo',
+          "productCategory": 'Men`s',
+          "productRating": 5,
+          "productPrice": 99.99,
+          "isNew": false,
+          "isExclusive": false,
+      },
+      1: {
+          "productId": 1,
+          "productImage": 'Images/p7.png',
+          "productName": 'Men`s FuelSell\nRebel',
+          "productCategory": 'Men`s',
+          "productRating": 5,
+          "productPrice": 129.99,
+          "isNew": false,
+          "isExclusive": false,
+      },
+      2: {
+          "productId": 2,
+          "productImage": 'Images/p6.png',
+          "productName": 'Side blocks\n1200',
+          "productCategory": 'Men`s',
+          "productRating": 5,
+          "productPrice": 139.99,
+          "isNew": false,
+          "isExclusive": false,
+      }
+    };
+  }
+}
