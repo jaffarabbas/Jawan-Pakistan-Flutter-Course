438b62862b3e9fa5a5461026ea5836d38efcd457
jaffarabbas <gamaportal8@gmail.com>
2021-10-18 01:16:45 +0500

    18/10/2021 01:16:37

--- Dart-Experiments/json_as_a_map.dart
+++ Dart-Experiments/json_as_a_map.dart
@@ -1,51 +1,62 @@
 // ignore_for_file: unused_local_variable
 
 void main() {
-  Map map = {
-    "friendsList": {
-      0: {
-        "firendId": 1,
-        "image": "",
-        "username": "gfg",
-      },
-      1: {
-        "firendId": 2,
-        "image": "",
-        "username": "Ahmed",
-      }
-    },
-    "chatList": {
-      1: {
-        "mainUser": {
-          "firendId": 1,
-          "image": "",
-          "username": "gfg",
-        },
-        "chatUser": {
-          "firendId": 2,
-          "image": "",
-          "username": "",
-        },
-        "messsges": {
-          "mainUser+messageid": {
-            "message": "",
-            "timestamps": "",
-          },
-          "chatUser+messageid": {
-            "message": "",
-            "timestamps": "",
-          }
-        }
-      }
-    }
-  };
-
+  // Map map = {
+  //   "friendsList": {
+  //     0: {
+  //       "firendId": 1,
+  //       "image": "",
+  //       "username": "gfg",
+  //     },
+  //     1: {
+  //       "firendId": 2,
+  //       "image": "",
+  //       "username": "Ahmed",
+  //     }
+  //   },
+  //   "chatList": {
+  //     1: {
+  //       "mainUser": {
+  //         "firendId": 1,
+  //         "image": "",
+  //         "username": "gfg",
+  //       },
+  //       "chatUser": {
+  //         "firendId": 2,
+  //         "image": "",
+  //         "username": "",
+  //       },
+  //       "messsges": {
+  //         "mainUser+messageid": {
+  //           "message": "",
+  //           "timestamps": "",
+  //         },
+  //         "chatUser+messageid": {
+  //           "message": "",
+  //           "timestamps": "",
+  //         }
+  //       }
+  //     }
+  //   }
+  // };
+  // print(map["friendsList"].length);
   // map.forEach((key, value) {
   //   print(value);
   // });
  // print(map["friendsList"][1]["username"]);
 
-  for(int i = 0; i < map["friendsList"].length;i++){
-     print(map["friendsList"][i]);
-  }
+ Map map ={
+   "friendsList":{
+
+   }
+ };
+ for (int i = 0; i < 10 ; i++){
+   map["friendsList"][map["friendsList"].length] = {
+   'name':'asdasd'
+ };
+ }
+ print(map["friendsList"]);
+  // for(int i = 0; i < map["friendsList"].length;i++){
+  //    print(map["friendsList"][i]);
+  // }
 }
