6e4f6da9c91256981d9bf6cff456b31e61472034
jaffarabbas <gamaportal8@gmail.com>
2021-10-25 17:51:45 +0500

    25/10/2021 17:51:40

---
+++ Flutter-Practice/flutter_app_4/lib/work_1/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            color: Colors.black
+          ),
+        ],
+      ),
+    );
+  }
+}
