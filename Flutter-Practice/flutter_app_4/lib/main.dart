9ad4bff3ac7d5c776fbc121d9f98c8b2e5e2435d
jaffarabbas <gamaportal8@gmail.com>
2021-10-25 17:51:44 +0500

    25/10/2021 17:51:40

---
+++ Flutter-Practice/flutter_app_4/lib/main.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app_4/work_1/expanded.dart';
+import 'package:flutter_app_4/work_1/home.dart';
+import 'package:flutter_app_4/work_2/mediaquery.dart';
+import 'package:flutter_app_4/work_2/multiplewidgitswithexpaneded.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      home: MediaQueryCheck(),
+    );
+  }
+}
