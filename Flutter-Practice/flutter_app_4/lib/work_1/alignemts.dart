9bfb46713ca6c5085c9426569496525637f175b5
jaffarabbas <gamaportal8@gmail.com>
2021-10-25 17:51:44 +0500

    25/10/2021 17:51:40

---
+++ Flutter-Practice/flutter_app_4/lib/work_1/alignemts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CrosMain extends StatefulWidget {
+  const CrosMain({ Key? key }) : super(key: key);
+
+  @override
+  _CrosMainState createState() => _CrosMainState();
+}
+
+class _CrosMainState extends State<CrosMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
