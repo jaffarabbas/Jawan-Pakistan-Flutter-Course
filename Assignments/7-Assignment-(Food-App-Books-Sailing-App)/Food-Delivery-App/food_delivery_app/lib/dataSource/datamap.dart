16e9cd2ba9091bfae5baf281ff4927189f6cb26f
jaffarabbas <gamaportal8@gmail.com>
2021-10-29 23:47:10 +0500

    29/10/2021 23:47:10

--- Assignments/7-Assignment-(Food-App-Books-Sailing-App)/Food-Delivery-App/food_delivery_app/lib/dataSource/datamap.dart
+++ Assignments/7-Assignment-(Food-App-Books-Sailing-App)/Food-Delivery-App/food_delivery_app/lib/dataSource/datamap.dart
@@ -32,8 +32,38 @@ class Datamap {
           "image": "assets/Images/roundImg1.png",
           "description": "With Extra Souse",
           "rating": 6
+        },
+        {
+          "name": "Spicy Chicken Noodles",
+          "price": 7.9,
+          "image": "assets/Images/roundImage2.png",
+          "description": "With Extra Chicken",
+          "rating": 5
+        },
+        {
+          "name": "Spicy Cheese Pizza",
+          "price": 9.9,
+          "image": "assets/Images/roundImage3.png",
+          "description": "With Extra Cheese",
+          "rating": 6
         }
       ],
+      "otherFoods":[
+        {
+          "name": "Chinnese Pizza",
+          "price": 6.9,
+          "image": "assets/Images/pizza.jpg",
+          "description": "With Extra Souse",
+          "rating": 6
+        },
+        {
+          "name": "Japanies Sochie",
+          "price": 8.9,
+          "image": "assets/Images/japanese-restaurant.jpg",
+          "description": "With Tuna",
+          "rating": 6
+        },
+      ]
     };
   }
 }
