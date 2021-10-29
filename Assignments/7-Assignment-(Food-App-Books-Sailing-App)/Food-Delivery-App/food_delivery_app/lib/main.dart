652a7b55bb30abd86b0cbb9315339500494f3d20
jaffarabbas <gamaportal8@gmail.com>
2021-10-29 23:47:10 +0500

    29/10/2021 23:47:10

--- Assignments/7-Assignment-(Food-App-Books-Sailing-App)/Food-Delivery-App/food_delivery_app/lib/main.dart
+++ Assignments/7-Assignment-(Food-App-Books-Sailing-App)/Food-Delivery-App/food_delivery_app/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/pages/home_page.dart';
@@ -8,13 +8,14 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  MyApp({ Key? key }) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  int currentBottomNavigationIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,7 +24,63 @@ class _MyAppState extends State<MyApp> {
         fontFamily: GoogleFonts.poppins().fontFamily,
       ),
       home: Material(
-        child: HomePage(),
+        child: Scaffold(
+          body: HomePage(),
+          bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) => setState(() => currentBottomNavigationIndex = index),
+          currentIndex: currentBottomNavigationIndex,
+          type: BottomNavigationBarType.fixed,
+          // fixedColor: Colors.red,
+          selectedLabelStyle: TextStyle(
+            fontWeight: FontWeight.w600,
+          ),
+          unselectedLabelStyle: TextStyle(
+            fontWeight: FontWeight.w600,
+          ),
+          // selectedItemColor: Colors.red,
+          // unselectedItemColor: Colors.red,
+          selectedIconTheme: IconThemeData(
+            color: Colors.red
+          ),
+          unselectedIconTheme: IconThemeData(
+            color: Colors.grey[400]
+          ),
+          selectedFontSize: 12,
+          unselectedFontSize: 12,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  FontAwesomeIcons.solidCompass,
+                  size: 20,
+                ),
+                label: 'search',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  FontAwesomeIcons.solidBookmark,
+                  size: 20,
+                ),
+                label: 'bookmark',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  FontAwesomeIcons.history,
+                  size: 20,
+                ),
+                label: 'history',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.notifications_active_sharp,
+                  size: 25,
+                ),
+                label: 'notification',
+            ),
+          ],
+        ),
+        ),
       ),
     );
   }
