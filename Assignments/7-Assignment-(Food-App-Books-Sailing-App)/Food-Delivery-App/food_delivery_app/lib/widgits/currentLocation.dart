9e4ea0d191cf9f6a6ca88e76679723dfa1e8f2c1
jaffarabbas <gamaportal8@gmail.com>
2021-10-29 23:47:12 +0500

    29/10/2021 23:47:10

--- Assignments/7-Assignment-(Food-App-Books-Sailing-App)/Food-Delivery-App/food_delivery_app/lib/widgits/currentLocation.dart
+++ Assignments/7-Assignment-(Food-App-Books-Sailing-App)/Food-Delivery-App/food_delivery_app/lib/widgits/currentLocation.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class CurrentLocation extends StatefulWidget {
   String locationName;
-  CurrentLocation({ Key? key, required this.locationName}) : super(key: key);
+  CurrentLocation({Key? key, required this.locationName}) : super(key: key);
 
   @override
   _CurrentLocationState createState() => _CurrentLocationState();
@@ -13,34 +13,65 @@ class CurrentLocation extends StatefulWidget {
 class _CurrentLocationState extends State<CurrentLocation> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
       children: [
-        Icon(
-          Icons.location_pin,
-          color: Colors.red,
-          size: 25,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.location_pin,
+              color: Colors.red,
+              size: 25,
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Text(
+              widget.locationName,
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1,
+                  color: Colors.grey[800],
+                  fontSize: 17),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Icon(
+              Icons.keyboard_arrow_down_rounded,
+              color: Colors.grey[500],
+              size: 30,
+            ),
+          ],
         ),
-        SizedBox(
-          width: 15,
-        ),
-        Text(
-          widget.locationName,
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-            letterSpacing: 1,
-            color: Colors.grey[800],
-            fontSize: 17
+        Container(
+          height: 40,
+          margin: EdgeInsets.symmetric(vertical: 15),
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: TextField(
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25.0),
+                borderSide: BorderSide(color: Color(0xfff0e6d1), width: 0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25.0),
+                borderSide: BorderSide(color: Color(0xfff0e6d1), width: 0),
+              ),
+              filled: true,
+              hintStyle: TextStyle(
+                  color: Colors.grey[500],
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold),
+              hintText: "Search Your Food",
+              fillColor: Color(0xfff0e6d1),
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.grey[500],
+              ),
+            ),
           ),
-        ),
-        SizedBox(
-          width: 10,
-        ),
-        Icon(
-          Icons.keyboard_arrow_down_rounded,
-          color: Colors.grey[500],
-          size: 30,
-        ),
+        )
       ],
     );
   }
