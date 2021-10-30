ba2a9bbef2bdd66f96ffe136248aae9dc6bba96b
jaffarabbas <gamaportal8@gmail.com>
2021-10-30 22:17:18 +0500

    30/10/2021 22:17:16

--- Assignments/7-Assignment-(Food-App-Books-Sailing-App)/Books-Sailing-App/flutter_book_sailing_app/lib/main.dart
+++ Assignments/7-Assignment-(Food-App-Books-Sailing-App)/Books-Sailing-App/flutter_book_sailing_app/lib/main.dart
@@ -1,115 +1,119 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_book_sailing_app/dataSource/datamap.dart';
+import 'package:flutter_book_sailing_app/pages/book_info_page.dart';
+import 'package:flutter_book_sailing_app/pages/home_page.dart';
+import 'package:flutter_book_sailing_app/utils/routes.dart';
+import 'package:flutter_book_sailing_app/widgits/theme.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentBottomNavigationIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.rubik().fontFamily,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Material(
+        child: Scaffold(
+          backgroundColor: AppTheme.primaryBackgroundColor,
+          appBar: AppBar(
+            toolbarHeight: 100,
+            leadingWidth: 70,
+            backgroundColor: AppTheme.primaryBackgroundColor,
+            elevation: 0,
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Builder(
+                builder: (context) => IconButton(
+                  icon: SvgPicture.asset(
+                    "assets/Icons/short_text.svg",
+                  ),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 35.0),
+                child: Icon(
+                  Icons.search,
+                  color: AppTheme.primaryFontcolor,
+                  size: 25,
+                ),
+              ),
+            ],
+          ),
+          //********Body********
+          body: HomePage(),
+          drawer: Drawer(),
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: (index) =>
+                setState(() => currentBottomNavigationIndex = index),
+            currentIndex: currentBottomNavigationIndex,
+            type: BottomNavigationBarType.fixed,
+            selectedLabelStyle: TextStyle(
+              fontWeight: FontWeight.w600,
             ),
-          ],
+            unselectedLabelStyle: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+            selectedIconTheme: IconThemeData(color: AppTheme.primaryColor),
+            unselectedIconTheme: IconThemeData(color: Colors.grey[400]),
+            selectedFontSize: 12,
+            unselectedFontSize: 12,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_filled,
+                  size: 20,
+                ),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  FontAwesomeIcons.bookmark,
+                  size: 20,
+                ),
+                label: 'bookmark',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.shopping_bag_rounded,
+                  size: 20,
+                ),
+                label: 'shoppingbag',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.tune,
+                  size: 25,
+                ),
+                label: 'setting',
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
