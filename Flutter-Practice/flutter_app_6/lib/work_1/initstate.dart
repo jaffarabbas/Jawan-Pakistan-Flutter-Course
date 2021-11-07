dfa2732571b0c86d3163c933097c144e1708a737
jaffarabbas <gamaportal8@gmail.com>
2021-11-07 20:54:34 +0500

    07/11/2021 20:54:28

---
+++ Flutter-Practice/flutter_app_6/lib/work_1/initstate.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+
+class Initiate extends StatefulWidget {
+  Initiate({ Key? key }) : super(key: key);
+
+  @override
+  _InitiateState createState() => _InitiateState();
+}
+
+class _InitiateState extends State<Initiate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
