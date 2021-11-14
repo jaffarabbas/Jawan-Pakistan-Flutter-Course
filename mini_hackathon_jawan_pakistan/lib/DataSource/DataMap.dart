c0edb5c2ee0809a596c0fcb87d7d98df1beb9238
jaffarabbas <gamaportal8@gmail.com>
2021-11-14 18:38:20 +0500

    14/11/2021 18:38:20

--- mini_hackathon_jawan_pakistan/lib/DataSource/DataMap.dart
+++ mini_hackathon_jawan_pakistan/lib/DataSource/DataMap.dart
@@ -32,35 +32,31 @@ class Datamap {
       "items": [
         {
           "productId": 0,
-          "productImage": 'images/p3.png',
+          "productImage": 'images/p1.png',
           "productName": 'Men`s\nFuelSell Echo',
-          "productCategory": 'FeMale`s',
-          "productRating": 5,
           "productPrice": 99.99,
           "isNew": false,
-          "isExclusive": false,
         },
         {
           "productId": 1,
-          "productImage": 'images/p7.png',
+          "productImage": 'images/p2.png',
           "productName": 'Men`s FuelSell\nRebel',
-          "productCategory": 'Men`s',
-          "productRating": 5,
+          "description":"",
           "productPrice": 129.99,
           "isNew": true,
-          "isExclusive": false,
         },
         {
           "productId": 2,
-          "productImage": 'images/p6.png',
+          "productImage": 'images/p3.png',
           "productName": 'Side blocks\n1200',
-          "productCategory": 'Men`s',
-          "productRating": 5,
+          "description":"",
           "productPrice": 139.99,
           "isNew": false,
           "isExclusive": true,
         }
       ],
+      "cartList":[],
+      "favourate":[]
     };
   }
 }
