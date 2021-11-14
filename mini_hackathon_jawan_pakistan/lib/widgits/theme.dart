885ab5405cb79af57d1a2fb4cbbccd8da94390b8
jaffarabbas <gamaportal8@gmail.com>
2021-11-14 17:12:28 +0500

    14/11/2021 17:12:09

---
+++ mini_hackathon_jawan_pakistan/lib/widgits/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTheme{
+  static const Color primarybackground = Color(0xff6441A5);
+  static const Color headingbackground = Color(0xff252525);
+  // static const Color iconbackground = Color(0xff3B3B3B);
+  // static const Color dateColor = Color(0xff676767);
+  // static const Color headingFontColor = Color(0xffffffff);
+  // static const Color paragraphColor = Color(0xffDFDCDC);
+}
