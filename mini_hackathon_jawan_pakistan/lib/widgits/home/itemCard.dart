01bea3e932be601c9728cf6f0ad8953e4ad51b11
jaffarabbas <gamaportal8@gmail.com>
2021-11-14 18:38:20 +0500

    14/11/2021 18:38:20

--- mini_hackathon_jawan_pakistan/lib/widgits/home/itemCard.dart
+++ mini_hackathon_jawan_pakistan/lib/widgits/home/itemCard.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: file_names, deprecated_member_use, must_be_immutable, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:mini_hackathon_jawan_pakistan/DataSource/DataMap.dart';
+import 'package:mini_hackathon_jawan_pakistan/widgits/theme.dart';
 
 class ItemCard extends StatefulWidget {
   Map productInformation;
-  ItemCard({Key? key,required this.productInformation}) : super(key: key);
+  ItemCard({Key? key, required this.productInformation}) : super(key: key);
 
   @override
   _ItemCardState createState() => _ItemCardState();
@@ -13,19 +15,28 @@ class ItemCard extends StatefulWidget {
 class _ItemCardState extends State<ItemCard> {
   @override
   Widget build(BuildContext context) {
-    var FontAwesomeIcons;
+
+    void AddToCart(){
+      setState(() {
+        Map<dynamic, dynamic> item = widget.productInformation;
+        print(widget.productInformation);
+        Datamap.DataSource()["cartList" as List<dynamic>][Datamap.DataSource()["cartList"].length].add(item);
+        print(Datamap.DataSource()["cartList"]);
+      });
+    } 
+
     return Container(
-      padding: EdgeInsets.all(30),
       margin: EdgeInsets.only(bottom: 15),
+      alignment: Alignment.center,
       child: Stack(
         overflow: Overflow.visible,
         children: [
           Center(
             child: Container(
               width: 280,
-              padding: EdgeInsets.only(left: 20),
-              margin: EdgeInsets.only(top: 19),
-              height: 130,
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 10),
+              height: 290,
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(10),
@@ -40,50 +51,69 @@ class _ItemCardState extends State<ItemCard> {
               ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: widget.productInformation['isNew']
-                    ? CrossAxisAlignment.start
-                    : CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Row(
-                    children: [
-                      Image.asset(
-                        widget.productInformation["productImage"],
-                        width: 90,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.productInformation["productName"],
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12,
+                  Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.favorite,
+                                color: AppTheme.primarybackground,
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 13,
-                          ),
-                          Text(
-                            widget.productInformation["productCategory"],
-                            style: TextStyle(
-                              color: Colors.grey[500],
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12,
+                            IconButton(
+                              onPressed: () {
+                                AddToCart();
+                              },
+                              icon: Icon(
+                                Icons.shopping_cart_rounded,
+                                color: AppTheme.primarybackground,
+                              ),
                             ),
-                          ),
-                          Text(
-                            '\$ ${widget.productInformation["productPrice"]}',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
+                          ],
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Image.asset(
+                          widget.productInformation["productImage"],
+                          width: 180,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              widget.productInformation["productName"],
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              ),
                             ),
-                          )
-                        ],
-                      ),
-                    ],
+                            SizedBox(
+                              height: 13,
+                            ),
+                            Text(
+                              '\$ ${widget.productInformation["productPrice"]}',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
                   )
                 ],
               ),
