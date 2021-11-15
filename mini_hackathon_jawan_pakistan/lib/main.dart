90b32e8d639f03ad8962b695da6a4a9638519696
jaffarabbas <gamaportal8@gmail.com>
2021-11-16 00:13:05 +0500

    16/11/2021 00:13:04

--- mini_hackathon_jawan_pakistan/lib/main.dart
+++ mini_hackathon_jawan_pakistan/lib/main.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mini_hackathon_jawan_pakistan/pages/about.dart';
 import 'package:mini_hackathon_jawan_pakistan/pages/home.dart';
 import 'package:mini_hackathon_jawan_pakistan/pages/login.dart';
+import 'package:mini_hackathon_jawan_pakistan/pages/search.dart';
+import 'package:mini_hackathon_jawan_pakistan/pages/splash.dart';
  
 void main() => runApp(MyApp());
  
