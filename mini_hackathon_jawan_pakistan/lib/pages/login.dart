ea67edfcdbae4c57b37b9f649ac916153bf2f7e3
jaffarabbas <gamaportal8@gmail.com>
2021-11-18 18:01:56 +0500

    18/11/2021 18:01:54

--- mini_hackathon_jawan_pakistan/lib/pages/login.dart
+++ mini_hackathon_jawan_pakistan/lib/pages/login.dart
@@ -14,11 +14,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool stateOfContainer = false;
-  @override
-  Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
-    
-    
+  final _formKey = GlobalKey<FormState>();
+
   ValidateFeilds(BuildContext context) async {
     if (_formKey.currentState!.validate()) {
       setState(() {
@@ -35,6 +32,8 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Stack(
@@ -62,33 +61,36 @@ class _LoginPageState extends State<LoginPage> {
                           // controller: passController,
                           hint: "Enter Password",
                           isPass: true),
-                      Material(
-                        color: stateOfContainer
-                            ? Colors.green[300]
-                            : AppTheme.primarybackground,
-                        borderRadius: BorderRadius.circular(
-                          stateOfContainer ? 80 : 12,
-                        ),
-                        child: InkWell(
-                          onTap: () => ValidateFeilds(context),
-                          child: AnimatedContainer(
-                            duration: Duration(seconds: 1),
-                            height: 50,
-                            width: stateOfContainer ? 50 : 200,
-                            alignment: Alignment.center,
-                            child: stateOfContainer
-                                ? Icon(
-                                    Icons.done,
-                                    color: Colors.white,
-                                  )
-                                : Text(
-                                    'LOGIN HERE',
-                                    style: TextStyle(
+                      Container(
+                        margin: EdgeInsets.only(top:30),
+                        child: Material(
+                          color: stateOfContainer
+                              ? Colors.green[300]
+                              : AppTheme.primarybackground,
+                          borderRadius: BorderRadius.circular(
+                            stateOfContainer ? 80 : 12,
+                          ),
+                          child: InkWell(
+                            onTap: () => ValidateFeilds(context),
+                            child: AnimatedContainer(
+                              duration: Duration(seconds: 1),
+                              height: 50,
+                              width: stateOfContainer ? 50 : 250,
+                              alignment: Alignment.center,
+                              child: stateOfContainer
+                                  ? Icon(
+                                      Icons.done,
                                       color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: stateOfContainer ? 12 : 15,
+                                    )
+                                  : Text(
+                                      'LOGIN HERE',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: stateOfContainer ? 12 : 15,
+                                      ),
                                     ),
-                                  ),
+                            ),
                           ),
                         ),
                       ),
