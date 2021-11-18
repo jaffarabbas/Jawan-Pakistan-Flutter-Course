353c1eb2c3c416e1b7d4830398b671c194c9dbfc
jaffarabbas <gamaportal8@gmail.com>
2021-11-18 18:01:59 +0500

    18/11/2021 18:01:54

--- mini_hackathon_jawan_pakistan/lib/pages/profile.dart
+++ mini_hackathon_jawan_pakistan/lib/pages/profile.dart
@@ -1,12 +1,74 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:mini_hackathon_jawan_pakistan/widgits/theme.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: AppTheme.primarybackground,
+              width: 400,
+              height: 300,
+              child: Container(
+                padding: EdgeInsets.all(50),
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.person,
+                    color: AppTheme.primarybackground,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+            ListView(
+              shrinkWrap: true,
+              children: [
+                ListTile(
+                  title: Text(
+                    "Email",
+                  ),
+                  subtitle: Text(
+                    "gamaportal8@gmail.com"
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "Phone Number",
+                  ),
+                  subtitle: Text(
+                    "03022988820"
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "Date of Birth",
+                  ),
+                  subtitle: Text(
+                    "6-1-2000"
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "City",
+                  ),
+                  subtitle: Text(
+                    "Karachi"
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
