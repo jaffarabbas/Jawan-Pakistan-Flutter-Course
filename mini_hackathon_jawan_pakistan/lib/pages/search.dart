0a92ff6974697ab4729427c9679cf50d92e863d0
jaffarabbas <gamaportal8@gmail.com>
2021-11-18 18:01:59 +0500

    18/11/2021 18:01:54

--- mini_hackathon_jawan_pakistan/lib/pages/search.dart
+++ mini_hackathon_jawan_pakistan/lib/pages/search.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:mini_hackathon_jawan_pakistan/pages/home.dart';
 import 'package:mini_hackathon_jawan_pakistan/widgits/theme.dart';
 
 class SearchPage extends StatelessWidget {
@@ -13,19 +14,30 @@ class SearchPage extends StatelessWidget {
         child: Container(
           height: 50,
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Padding(
-                padding: EdgeInsets.all(5),
-                child: Icon(Icons.close_rounded, color: AppTheme.primarybackground,),
-              ),
+                  padding: EdgeInsets.all(5),
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomePage()),
+                      );
+                    },
+                    icon: Icon(
+                      Icons.close_rounded,
+                      color: AppTheme.primarybackground,
+                    ),
+                  )),
               Container(
                 width: 300,
                 height: 40,
+                padding: EdgeInsets.only(top: 17),
                 child: TextField(
                     decoration: InputDecoration(
-                    enabledBorder:  OutlineInputBorder(
-                        borderSide: BorderSide(width: 0, color: Colors.white) 
-                    ),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 0, color: Colors.white)),
                   hintText: "Enter Name or Address",
                   hintStyle: TextStyle(
                     color: AppTheme.primarybackground,
