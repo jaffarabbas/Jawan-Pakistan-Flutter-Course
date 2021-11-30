532c99dfcc57f1b7fccf9e67ae6ea41ee5db1928
Jaffar Abbas <gamaportal8@gmail.com>
2021-11-30 19:29:08 +0500

    30/11/2021 19:29:08

--- Flutter-Practice/flutter_app_7/lib/pages/home_page.dart
+++ Flutter-Practice/flutter_app_7/lib/pages/home_page.dart
@@ -1,8 +1,13 @@
+// ignore_for_file: unnecessary_new, curly_braces_in_flow_control_structures
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_app_7/models/User.dart';
 import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
-  HomePage({ Key? key }) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -11,12 +16,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   getUser() async {
     var userList = [];
-    var user = await http.get(Uri.http("jsonplaceholder.typicode.com", "users"));
+    var responce =
+        await http.get(Uri.http("jsonplaceholder.typicode.com", "users"));
+    var jsonCode = jsonDecode(responce.body);
+    for (var i in jsonCode) {
+      User u = new User(i["name"], i["email"]);
+      userList.add(u);
+    }
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: FutureBuilder(
+        future: getUser(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text("${snapshot.data![index].name}"),
+                  subtitle: Text("${snapshot.data![index].email}"),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
