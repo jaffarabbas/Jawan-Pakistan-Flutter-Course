c0ebc10278db244cf7da46f776bc61374f06aa9b
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-01 20:50:00 +0500

    01/12/2021 20:49:59

--- Flutter-Practice/flutter_app_7/lib/models/User.dart
+++ Flutter-Practice/flutter_app_7/lib/models/User.dart
@@ -1,5 +1,21 @@
-class User{
-  String name;
-  String email;
-  User(this.name,this.email);
+// ignore_for_file: avoid_function_literals_in_foreach_calls
+
+class Users{
+  late String name;
+  late String email;
+  //assiging constructor parameter
+  Users({required this.name,required this.email});
+  //
+  Users.fromJson(Map<String,dynamic> json){
+    name = json['name'];
+    email = json['email'];
+  }
+  //
+  static List<Users> fromJsonList(List<dynamic> jsonList){
+    List<Users> users = [];
+    jsonList.forEach((element) {
+      users.add(Users.fromJson(element));
+    });
+    return users;
+  }
 }
