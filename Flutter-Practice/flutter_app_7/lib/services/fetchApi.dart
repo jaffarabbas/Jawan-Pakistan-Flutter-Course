dc3d984636ff0cf52967a2237fb161c2af8db335
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-01 20:50:00 +0500

    01/12/2021 20:49:59

--- Flutter-Practice/flutter_app_7/lib/services/fetchApi.dart
+++ Flutter-Practice/flutter_app_7/lib/services/fetchApi.dart
@@ -1,6 +1,14 @@
 import 'dart:convert';
+import 'package:flutter_app_7/models/User.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService{
-
+  Future<List<Users>> fetchData() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    if(response.statusCode == 200){
+      return Users.fromJsonList(jsonDecode(response.body));
+    }else{
+      throw Exception('Failed to load Users');
+    }
+  }
 }
