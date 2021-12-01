a50d46960aef276a0362cd987a92f2ea63943aff
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-01 23:01:49 +0500

    01/12/2021 23:01:47

--- Flutter-Practice/flutter_app_7/lib/services/fetchApi.dart
+++ Flutter-Practice/flutter_app_7/lib/services/fetchApi.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:flutter_app_7/models/User.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService{
-  Future<List<Users>> fetchData() async {
+  Future<List<Users>> fetchData(bool state) async {
     final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    final loadBundle = await rootBundle.loadString('assets/files/data.json');
     if(response.statusCode == 200){
-      return Users.fromJsonList(jsonDecode(response.body));
+      return Users.fromJsonList(jsonDecode(state ? response.body : loadBundle));
     }else{
       throw Exception('Failed to load Users');
     }
