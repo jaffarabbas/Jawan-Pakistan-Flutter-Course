9e09aa61e375771848967a486fe12f5cc26f2de4
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-02 09:05:36 +0500

    02/12/2021 09:05:34

---
+++ Assignments/9-Assignment-(Weather-App-Using-Weather-API)/weather_app/lib/main.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:weather_app/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
