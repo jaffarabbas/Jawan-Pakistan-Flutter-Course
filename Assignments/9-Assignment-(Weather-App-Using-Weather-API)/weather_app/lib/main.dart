968728d72ae03db333321e821d04ebf5538c438a
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-05 00:07:12 +0500

    05/12/2021 00:07:10

--- Assignments/9-Assignment-(Weather-App-Using-Weather-API)/weather_app/lib/main.dart
+++ Assignments/9-Assignment-(Weather-App-Using-Weather-API)/weather_app/lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/pages/splash_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: GoogleFonts.poppins().fontFamily,
       ),
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
