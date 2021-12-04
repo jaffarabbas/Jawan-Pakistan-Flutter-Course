405971bd1335b032a382d0e110e6b519a732de44
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-04 22:37:16 +0500

    04/12/2021 22:37:04

--- Assignments/9-Assignment-(Weather-App-Using-Weather-API)/weather_app/lib/models/model.dart
+++ Assignments/9-Assignment-(Weather-App-Using-Weather-API)/weather_app/lib/models/model.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
 
 class WeatherModel {
-  // late num id;
+  late num id;
   // late String country;
   late String name;
   // late num timezone;
   // late num sunrise;
   // late num sunset;
-  // late String main;
+  late String main;
   // late String description;
   // late String icon;
   late num temp;
@@ -15,21 +15,21 @@ class WeatherModel {
   // late num temp_min;
   // late num temp_max;
   // late num pressure;
-  // late num humidity;
-  // late num speed;
-  // late num deg;
+  late num humidity;
+  late num speed;
+  late num deg;
   // late num all;
   // late num lon;
   // late num lat;
 
   WeatherModel({
-    // required this.id,
+    required this.id,
     // required this.country,
     required this.name,
     // required this.timezone,
     // required this.sunrise,
     // required this.sunset,
-    // required this.main,
+    required this.main,
     // required this.description,
     // required this.icon,
     required this.temp,
@@ -37,32 +37,32 @@ class WeatherModel {
     // required this.temp_min,
     // required this.temp_max,
     // required this.pressure,
-    // required this.humidity,
-    // required this.speed,
-    // required this.deg,
+    required this.humidity,
+    required this.speed,
+    required this.deg,
     // required this.all,
     // required this.lon,
     // required this.lat,
   });
 
   WeatherModel.fromJson(Map map) {
-    // id = map['id'];
+    id = map['id'];
     // country = map['country'];
     name = map['name'];
     // timezone = map['timezone'];
     // sunrise = map['sunrise'];
     // sunset = map['sunset'];
-    // main = map['main'];
+    main = map['weather'][0]['main'];
     // description = map['description'];
     // icon = map['icon'];
-    temp = map['temp'];
+    temp = map['main']['temp'];
     // feels_like = map['feels_like'];
     // temp_min = map['temp_min'];
     // temp_max = map['temp_max'];
     // pressure = map['pressure'];
-    // humidity = map['humidity'];
-    // speed = map['speed'];
-    // deg = map['deg'];
+    humidity = map['main']['humidity'];
+    speed = map['wind']['speed'];
+    deg = map['wind']['deg'];
     // all = map['all'];
     // lon = map['lon'];
     // lat = map['lat'];
