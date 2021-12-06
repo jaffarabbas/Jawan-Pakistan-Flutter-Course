0c7cfa62d6c48661afe214753f037c0953176d1a
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-06 15:06:59 +0500

    06/12/2021 15:06:42

---
+++ Flutter-Practice/flutter_app_8/lib/main.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_8/pages/home_page.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return Container(
+              child: Text("Error"),
+            );
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return HomePage();
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
+          return CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+}
