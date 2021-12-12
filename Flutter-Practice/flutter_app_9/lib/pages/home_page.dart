753f3fbf2604b71a87cad75de783ecc3309d4e8b
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-12 19:11:35 +0500

    12/12/2021 19:11:34

--- Flutter-Practice/flutter_app_9/lib/pages/home_page.dart
+++ Flutter-Practice/flutter_app_9/lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, unnecessary_new
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -12,12 +12,50 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController controller = new TextEditingController();
+  final Stream<QuerySnapshot> taskStream =
+      FirebaseFirestore.instance.collection('task').snapshots();
+  //collection refrence
+  CollectionReference taskRefrence = FirebaseFirestore.instance.collection('task');
+  List collections = [];
   addData() async {
     await FirebaseFirestore.instance.collection('task').add({
       'task': controller.value.text,
       'date':
           '${DateTime.now().year}-${DateTime.now().month}-${DateTime.now().day}-${DateTime.now()}'
     });
+    controller.clear();
+  }
+
+
+Future<void> _showMyDialog(String value, bool state) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(state ? 'Error Message' : 'Success Messages'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('$value'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> deleteTask(String id){
+    return taskRefrence.doc(id).delete();
   }
 
   @override
@@ -40,6 +78,7 @@ class _HomePageState extends State<HomePage> {
               child: ElevatedButton(
                 onPressed: () {
                   addData();
+                  _showMyDialog("Added Text", false);
                 },
                 child: Text(
                   "Insert Data",
@@ -48,14 +87,32 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             Container(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: 54,
-                itemBuilder: (context, index) {
-                  return CardItem();
-                },
-              ),
-            )
+                child: StreamBuilder<QuerySnapshot>(
+              stream: taskStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Something went wrong');
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                return ListView(
+                  shrinkWrap: true,
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    data["id"] = document.id;
+                    return CardItem(data,deleteTask);
+                  }).toList(),
+                );
+              },
+            ))
           ],
         ),
       ),
@@ -63,20 +120,20 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-Widget CardItem() => Card(
+Widget CardItem(Map data,Function delete) => Card(
         child: Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text("sad"),
+          Text("${data["task"]}",style: TextStyle(fontSize: 20)),
           Row(
             children: [
               Container(
                 child: ElevatedButton(
                   onPressed: () {},
                   child: Text(
-                    "Login",
+                    "Edit",
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
@@ -86,9 +143,11 @@ Widget CardItem() => Card(
               ),
               Container(
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    delete("${data["id"]}");
+                  },
                   child: Text(
-                    "Login",
+                    "Delete",
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
@@ -98,3 +157,4 @@ Widget CardItem() => Card(
         ],
       ),
     ));
+
