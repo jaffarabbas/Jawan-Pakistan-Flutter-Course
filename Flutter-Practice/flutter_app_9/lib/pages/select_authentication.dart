bbaf737446f4f8b7f57082df06049543c62927ee
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-22 00:07:13 +0500

    22/12/2021 00:06:32

--- Flutter-Practice/flutter_app_9/lib/pages/select_authentication.dart
+++ Flutter-Practice/flutter_app_9/lib/pages/select_authentication.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_9/pages/home_page.dart';
 import 'package:flutter_app_9/pages/login_page.dart';
 import 'package:flutter_app_9/pages/register_page.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthSelecter extends StatefulWidget {
   AuthSelecter({Key? key}) : super(key: key);
@@ -12,6 +15,20 @@ class AuthSelecter extends StatefulWidget {
 }
 
 class _AuthSelecterState extends State<AuthSelecter> {
+
+Future<UserCredential> signInWithGoogle() async {
+  // Create a new provider
+  GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+  googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+
+  // Or use signInWithRedirect
+  // return await FirebaseAuth.instance.signInWithRedirect(googleProvider);
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,6 +68,20 @@ class _AuthSelecterState extends State<AuthSelecter> {
                     );
                   },
                   child: Text("Login", style: TextStyle(color: Colors.white))),
+            ),
+            Container(
+              margin: EdgeInsets.only(top:20,bottom: 10),
+              child: ElevatedButton(
+                  onPressed: () {
+                    signInWithGoogle().then((value) => 
+                     Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomePage(user: FirebaseAuth.instance.currentUser as User),
+                      ),
+                    ),).catchError((e) => print(e));
+                  },
+                  child: Text("Google", style: TextStyle(color: Colors.white))),
             ),
           ],
         ),
