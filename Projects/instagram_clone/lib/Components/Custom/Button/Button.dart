a979cfd927c418135babf94253a39ebedb706a33
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-22 00:06:53 +0500

    22/12/2021 00:06:32

---
+++ Projects/instagram_clone/lib/Components/Custom/Button/Button.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: file_names
+
+import 'package:fb_login_app/Config/size_config.dart';
+import 'package:flutter/material.dart';
+
+class ButtonText extends StatelessWidget {
+  final void Function() function;
+  final String text;
+  const ButtonText({Key? key, required this.function, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: function,
+      child: Container(
+        alignment: Alignment.center,
+
+        height: getSize(false, 0.0525),
+        width: getSize(true, .7),
+        child: Text(text),
+      ),
+    );
+  }
+}
