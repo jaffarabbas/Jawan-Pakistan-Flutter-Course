576b4ba4229df0bae1426d101d594abaf4b19941
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-22 00:06:54 +0500

    22/12/2021 00:06:32

---
+++ Projects/instagram_clone/lib/Components/Header/MainHeader.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class MainHeader extends StatefulWidget {
+  const MainHeader({ Key? key }) : super(key: key);
+
+  @override
+  _MainHeaderState createState() => _MainHeaderState();
+}
+
+class _MainHeaderState extends State<MainHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
