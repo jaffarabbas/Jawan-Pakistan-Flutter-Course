5fe0c0df22a4d24f5d3637a76968710aadec034b
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-27 01:37:37 +0500

    27/12/2021 01:36:58

--- Projects/instagram_clone/lib/Pages/Login/SignIn/ForgetPassword.dart
+++ Projects/instagram_clone/lib/Pages/Login/SignIn/ForgetPassword.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: file_names
 
-import 'package:fb_login_app/Components/Custom/Button/ButtonColored.dart';
-import 'package:fb_login_app/Components/Custom/TextFeild/TextFeild_1.dart';
-import 'package:fb_login_app/Config/size_config.dart';
-import 'package:fb_login_app/Model/TextFeildModel.dart';
-import 'package:fb_login_app/Pages/Login/SignIn/SignInSrceen.dart';
+import 'package:instagram_clone/Components/Custom/Button/ButtonColored.dart';
+import 'package:instagram_clone/Components/Custom/TextFeild/TextFeild_1.dart';
+import 'package:instagram_clone/Config/size_config.dart';
+import 'package:instagram_clone/Model/TextFeildModel.dart';
+import 'package:instagram_clone/Pages/Login/SignIn/SignInSrceen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
