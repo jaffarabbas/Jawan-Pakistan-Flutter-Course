34ef78355a99cc84928cafa5da4c5e18313b801d
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-27 01:37:34 +0500

    27/12/2021 01:36:58

---
+++ Projects/instagram_clone/lib/Pages/Post/postCard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PostCard extends StatefulWidget {
+  PostCard({ Key? key }) : super(key: key);
+
+  @override
+  _PostCardState createState() => _PostCardState();
+}
+
+class _PostCardState extends State<PostCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
