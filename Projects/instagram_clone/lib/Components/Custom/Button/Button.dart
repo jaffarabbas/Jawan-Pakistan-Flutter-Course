3bdd06fe34336a9dbcb2d32e01fdf5b8dcd4f6cf
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-27 01:37:35 +0500

    27/12/2021 01:36:58

--- Projects/instagram_clone/lib/Components/Custom/Button/Button.dart
+++ Projects/instagram_clone/lib/Components/Custom/Button/Button.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: file_names
 
-import 'package:fb_login_app/Config/size_config.dart';
+import 'package:instagram_clone/Config/size_config.dart';
 import 'package:flutter/material.dart';
 
 class ButtonText extends StatelessWidget {
