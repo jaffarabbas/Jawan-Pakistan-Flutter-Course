0394f9f8ab83f60f24aaa48f9781124503c2d3fa
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-27 01:37:18 +0500

    27/12/2021 01:36:58

---
+++ Flutter-Practice/flutter_app_10/lib/pages/register_page.dart
@@ -0,0 +1,140 @@
+// ignore_for_file: prefer_const_constructors, unnecessary_new
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_10/pages/login_page.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController nameController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+
+  addData(String userId) async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .add({
+      'userId':userId,
+      'userEmail': emailController.value.text,
+      'username':nameController.value.text,
+    });
+  }
+
+  Future Register() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: emailController.value.text,
+              password: passwordController.value.text);
+      _showMyDialog('User Inserted Succesfully', false);
+      addData(userCredential.user!.uid);
+      emailController.clear();
+      nameController.clear();
+      passwordController.clear();
+       Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => LoginPage(),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        _showMyDialog('The password provided is too weak.', true);
+      } else if (e.code == 'email-already-in-use') {
+        _showMyDialog('The account already exists for that email.', true);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _showMyDialog(String value, bool state) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(state ? 'Error Message' : 'Success Messages'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('$value'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Register",
+              style: TextStyle(fontSize: 30, color: Colors.blueAccent),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Card(
+                child: TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(hintText: "Enter Email"),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Card(
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(hintText: "Enter Username"),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Card(
+                child: TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(hintText: "Enter Password"),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: ElevatedButton(
+                onPressed: () {
+                  Register();
+                  print("Done");
+                },
+                child: Text(
+                  "Regsiter",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
