2d2329a48f21596edb4a4fedb69a6c0a6f4df061
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-28 17:01:33 +0500

    28/12/2021 17:01:29

--- Flutter-Practice/flutter_app_10/lib/pages/select_authentication.dart
+++ Flutter-Practice/flutter_app_10/lib/pages/select_authentication.dart
@@ -15,20 +15,6 @@ class AuthSelecter extends StatefulWidget {
 }
 
 class _AuthSelecterState extends State<AuthSelecter> {
-
-Future<UserCredential> signInWithGoogle() async {
-  // Create a new provider
-  GoogleAuthProvider googleProvider = GoogleAuthProvider();
-
-  googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-
-  // Once signed in, return the UserCredential
-  return await FirebaseAuth.instance.signInWithPopup(googleProvider);
-
-  // Or use signInWithRedirect
-  // return await FirebaseAuth.instance.signInWithRedirect(googleProvider);
-}
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,51 +23,61 @@ Future<UserCredential> signInWithGoogle() async {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "Flutter CRUD App",
+              "Flutter Chat App",
               style: TextStyle(
                   fontSize: 30,
-                  color: Colors.blueAccent,
+                  color: Colors.green,
                   fontWeight: FontWeight.bold),
             ),
             Container(
-              margin: EdgeInsets.only(top:20,bottom: 10),
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => RegisterPage(),
-                      ),
-                    );
-                  },
-                  child:
-                      Text("Register", style: TextStyle(color: Colors.white))),
+              margin: EdgeInsets.only(top: 20, bottom: 10),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RegisterPage(),
+                    ),
+                  );
+                },
+                child: Container(
+                  margin: EdgeInsets.only(top: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 17),
+                  child: Text(
+                    "Resgister",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
             ),
             Container(
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => LoginPage(),
-                      ),
-                    );
-                  },
-                  child: Text("Login", style: TextStyle(color: Colors.white))),
-            ),
-            Container(
-              margin: EdgeInsets.only(top:20,bottom: 10),
-              child: ElevatedButton(
-                  onPressed: () {
-                    signInWithGoogle().then((value) => 
-                     Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => HomePage(user: FirebaseAuth.instance.currentUser as User),
-                      ),
-                    ),).catchError((e) => print(e));
-                  },
-                  child: Text("Google", style: TextStyle(color: Colors.white))),
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginPage(),
+                    ),
+                  );
+                },
+                child: Container(
+                  margin: EdgeInsets.only(top: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 17),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
