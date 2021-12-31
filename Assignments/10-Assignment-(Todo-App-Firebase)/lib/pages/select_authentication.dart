312eeeb78740b58da41f39686daa3344bcee1f6b
Jaffar Abbas <gamaportal8@gmail.com>
2021-12-31 18:22:10 +0500

    31/12/2021 18:21:45

---
+++ Assignments/10-Assignment-(Todo-App-Firebase)/lib/pages/select_authentication.dart
@@ -0,0 +1,91 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_9/pages/home_page.dart';
+import 'package:flutter_app_9/pages/login_page.dart';
+import 'package:flutter_app_9/pages/register_page.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthSelecter extends StatefulWidget {
+  AuthSelecter({Key? key}) : super(key: key);
+
+  @override
+  _AuthSelecterState createState() => _AuthSelecterState();
+}
+
+class _AuthSelecterState extends State<AuthSelecter> {
+
+Future<UserCredential> signInWithGoogle() async {
+  // Create a new provider
+  GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+  googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+
+  // Or use signInWithRedirect
+  // return await FirebaseAuth.instance.signInWithRedirect(googleProvider);
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Flutter CRUD App",
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold),
+            ),
+            Container(
+              margin: EdgeInsets.only(top:20,bottom: 10),
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RegisterPage(),
+                      ),
+                    );
+                  },
+                  child:
+                      Text("Register", style: TextStyle(color: Colors.white))),
+            ),
+            Container(
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginPage(),
+                      ),
+                    );
+                  },
+                  child: Text("Login", style: TextStyle(color: Colors.white))),
+            ),
+            Container(
+              margin: EdgeInsets.only(top:20,bottom: 10),
+              child: ElevatedButton(
+                  onPressed: () {
+                    signInWithGoogle().then((value) => 
+                     Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomePage(user: FirebaseAuth.instance.currentUser as User),
+                      ),
+                    ),).catchError((e) => print(e));
+                  },
+                  child: Text("Google", style: TextStyle(color: Colors.white))),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
