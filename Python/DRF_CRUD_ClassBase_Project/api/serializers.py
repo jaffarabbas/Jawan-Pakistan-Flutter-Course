7ed949a197fd632cc3afe3a7243e38d6320fe5e1
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-15 18:50:19 +0500

    15/02/2022 18:49:51

---
+++ Python/DRF_CRUD_ClassBase_Project/api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Student
+
+class StudentSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    roll = serializers.IntegerField()
+    city = serializers.CharField(max_length=100)
+
+    def create(self,validated_data):
+        return Student.objects.create(**validated_data)
+    
+    def update(seld, instance, validated_data):
+        instance.name = validated_data.get('name',instance.name)
+        instance.roll = validated_data.get('roll',instance.roll)
+        instance.city = validated_data.get('city',instance.city)
+        instance.save()
+        return instance
