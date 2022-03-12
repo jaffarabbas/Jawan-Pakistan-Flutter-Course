d4f146c3a1f94f6f07c1a727c084a0470a388a0d
Jaffar Abbas <gamaportal8@gmail.com>
2022-03-12 19:45:28 +0500

    12/03/2022 19:44:10

---
+++ Flutter-Practice/flutter_app_11/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_11/screen/home/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       home: HomeView(),
+    );
+  }
+}
