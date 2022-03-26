36e3b221eb49620b1ccfb71a82f71d9dfeb731a8
Jaffar Abbas <gamaportal8@gmail.com>
2022-03-26 20:51:59 +0500

    26/03/2022 20:51:57

--- final_hackathon_app/lib/Services/API/getApi.dart
+++ final_hackathon_app/lib/Services/API/getApi.dart
@@ -6,15 +6,15 @@ import 'package:final_hackathon_app/Model/UserModel.dart';
 import 'package:final_hackathon_app/Services/API/API_Route.dart';
 import 'package:http/http.dart' as http;
 
-Future<dynamic> getApi(String apiRoute) async {
+class ApiService{
+  Future<dynamic> getApi(String apiRoute) async {
   final response = await http.get(Uri.parse(apiRoute));
-
   if (response.statusCode == 200) {
-    final items = json.decode(response.body);
-
+    final items = UserModel.fromJsonList(jsonDecode(response.body));;
     return (items);
   } else {
     print("not running because of error");
     throw Exception('Failed to load album');
   }
+}
 }
