f1b64df9098d3f4db073dbc1ce638a0de678c8b8
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-20 15:02:41 +0500

    Commited Flutter-Practice/flutter_app_9/test/widget_test.dart Flutter-Practice/flutter_app_9/test/widget_test.dart at 20-07-2022 03:02:40

--- Flutter-Practice/flutter_app_9/test/widget_test.dart
+++ Flutter-Practice/flutter_app_9/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_app_9/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
